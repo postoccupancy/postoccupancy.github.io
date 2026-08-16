d46002da071fd28baf1aad2757ef8dee826bb208
Adrian <adrmac@gmail.com>
2026-08-16 14:27:58 -0700

    added Astro build config via Github Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   
-  site: 'https://yourdomain.com',
+  site: 'https://postoccupancy.github.io',
+  base: '/',
   integrations: [sitemap()]
 });
